eb5a382dc8c8aa90999c49b7db1cadbc04728027
Hex <hecksadecimal@outlook.com>
2023-12-04 22:04:01 -0500

    Disable CSS compressor

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Newerparp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.css_compressor = nil
   end
 end
