cb4885652018b9046a8ad968a6367275549afc40
Hex <hecksadecimal@outlook.com>
2023-12-09 02:10:54 -0500

    Various bbcode fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,3 @@
 module ApplicationHelper
     include Pagy::Frontend
-
-    def lower_bbcode(input_text)
-        pattern = /\[([a-zA-Z]+)(=(?:[^\]]+))?]/i
-
-        result = input_text.gsub(pattern) do |match|
-            tag_name = $1.downcase  
-            argument = $2
-            "[#{tag_name}#{argument}]"
-        end
-
-        result
-    end
-
-    def raw_tag_sanitize(input_text)
-        input_text.gsub(/\[raw\](?<content>.*?)\[\/raw\]/im) do |match|
-            content = $~[:content]
-            "[raw]#{content.gsub(/\[/, '&#91;').gsub(/\]/, '&#93;')}[/raw]"
-        end
-    end
-
-    def raw_tag_desanitize(input_text)
-        input_text.gsub(/\<raw\>(?<content>.*?)\<\/raw\>/im) do |match|
-            content = $~[:content]
-            "<raw>#{content.gsub("&amp;#91;", "\[" ).gsub("&amp;#93;", "\]")}</raw>"
-        end
-    end
 end
