7baf1fb9f44eb8decda0340521d48b6d779a1f25
Hex <hecksadecimal@outlook.com>
2023-12-08 23:14:58 -0500

    Default unleash config to empty values

--- config/initializers/unleash.rb
+++ config/initializers/unleash.rb
@@ -1,8 +1,8 @@
 Unleash.configure do |config|
     config.app_name = "Newerparp"
-    config.url      = ENV.fetch("UNLEASH_API")
-    config.custom_http_headers = {'Authorization': ENV.fetch("UNLEASH_KEY")}
-    config.instance_id = ENV.fetch("UNLEASH_INSTANCE_ID")
+    config.url      = ENV.fetch("UNLEASH_API") { "" }
+    config.custom_http_headers = {'Authorization': ENV.fetch("UNLEASH_KEY") { "" }}
+    config.instance_id = ENV.fetch("UNLEASH_INSTANCE_ID") { "" }
     config.logger   = Rails.logger
 end
   
