06e2901a8f87f9cadac52a1347c7ea14f4ae3942
Hex <hecksadecimal@outlook.com>
2023-12-13 07:56:17 -0500

    Add topics and descriptions to chat rooms

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,21 @@ module.exports = {
     extend: {
       backgroundImage: {
         global: 'var(--background-image)'
-      }
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        }
+      },
     }
   },
   plugins: [
