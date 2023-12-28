ccd555169846bd92d87ea4281f7e26df1b10779f
Hex <hecksadecimal@outlook.com>
2023-12-28 14:21:23 -0500

    Add email changing and password changing to account settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,8 @@ class ApplicationController < ActionController::Base
     private
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:email])
+
         end
 
         def set_last_seen
