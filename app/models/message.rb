0fe7cdebd1ece5d8361e0b8b715b807ba89ff05d
Hex <hecksadecimal@outlook.com>
2023-12-29 13:47:46 -0500

    Fix image display in chats and image upload enable/disable

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,12 @@ class Message < ApplicationRecord
     belongs_to :account, :foreign_key => 'user_id'
     belongs_to :chat_user, query_constraints: [:chat_id, :user_id]
     belongs_to :chat
+    has_rich_text :content
+
+    enum render_mode: [ :bbcode, :actiontext ]
+
+    validate :attachments_valid
+    validate :has_some_content
 
     has_rich_text :content
 
@@ -18,5 +24,22 @@ class Message < ApplicationRecord
     }
     after_update_commit -> { broadcast_replace_to "chat_#{chat.id}" }
     after_destroy_commit -> { broadcast_remove_to "chat_#{chat.id}" }
+
+    def has_some_content
+        return if content? || (!text.nil? && !text.empty?)
+        errors.add(:content, "must not be empty")
+    end
+
+    def attachments_valid
+        if content
+            content.body.attachments.each do |attach|
+                if chat.group_chat? && !chat.group_chat.image_upload
+                    errors.add(:files, 'are disallowed in this room') unless chat.group_chat.image_upload
+                end
+                errors.add(:file, "#{attach.filename} is larger than 10 mb") if attach.byte_size > 1242880
+                errors.add(:file, "#{attach.filename} is not an image") unless attach.image? 
+                errors.add(:file, "#{attach.filename} is not a supported image type (png, jpeg)") unless attach.content_type == 'image/jpeg' || attach.content_type == 'image/png'
+            end
+        end
+    end
 end
-  
