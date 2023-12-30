50aaef6100eb4e47681e72c6468ff5e687a4bf70
Hex <hecksadecimal@outlook.com>
2023-12-29 21:22:33 -0500

    Autoscroll fixes

--- app/javascript/controllers/autoscroll_controller.js
+++ app/javascript/controllers/autoscroll_controller.js
@@ -3,14 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="autoscroll"
 export default class extends Controller {
   connect() {
-    this.element.scrollTo(0, this.element.scrollHeight);
+    window.setTimeout(() => {
+      this.element.lastElementChild.scrollIntoView(false);
+    }, 500)
+
     this.element.addEventListener(
       "DOMNodeInserted",
       (event) => {
-        var scrollVal = this.element.scrollHeight - this.element.scrollTop - this.element.clientHeight;
-        if (scrollVal < 30) {
-          this.element.scrollTo(0, this.element.scrollHeight);
-        }
+        window.setTimeout(() => {
+          var scrollPercentage = 100 * this.element.scrollTop / (this.element.scrollHeight-this.element.clientHeight); 
+          if (scrollPercentage > 50) {
+            this.element.lastElementChild.scrollIntoView(false);
+          }
+        }, 500)
       },
       false,
     );
