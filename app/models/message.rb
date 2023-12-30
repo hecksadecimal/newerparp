8f2df62fc80e53ad28336ee4201a9d1fd7e86883
Hex <hecksadecimal@outlook.com>
2023-12-29 20:46:43 -0500

    Remove empty message check

--- app/models/message.rb
+++ app/models/message.rb
@@ -13,7 +13,6 @@ class Message < ApplicationRecord
     enum render_mode: [ :bbcode, :actiontext ]
 
     validate :attachments_valid
-    validate :has_some_content
     validate :content_length
 
     has_rich_text :content
@@ -26,10 +25,6 @@ class Message < ApplicationRecord
     after_update_commit -> { broadcast_replace_to "chat_#{chat.id}" }
     after_destroy_commit -> { broadcast_remove_to "chat_#{chat.id}" }
 
-    def has_some_content
-        return if content? || (!text.nil? && !text.empty?) || (content.body && content.body.attachments.size > 0)
-        errors.add(:content, "must not be empty")
-    end
 
     def content_length
         return if (content? || text?) && content.to_s.size < 20000
